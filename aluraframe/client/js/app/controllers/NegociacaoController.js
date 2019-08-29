e0de13e6ada21edcfd4a5d84d79f647ff19d932e
Eduardo Fernando Dias <dudu.dias74@gmail.com>
2019-08-29 18:02:21 -0300

    primeiro commit

---
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -0,0 +1,20 @@
+class NegociacaoController{
+    constructor(){
+        let $ = document.querySelector.bind(document)
+        this._inputData = $('#data');
+        this._inputQuantidade = $('#quantidade');
+        this._inputValor = $('#valor');
+    }
+    
+    adiciona(event){
+        event.preventDefault();
+        let data = new Date(this._inputData.value.split('-'));
+        let negocicao = new Negociacao(
+            data,
+            this._inputQuantidade.value,
+            this._inputValor.value
+        );
+    console.log(negocicao);    
+        
+    }
+}
