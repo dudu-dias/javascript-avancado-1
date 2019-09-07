239d9e1eaa4c9dd6ee81f454727b6457919fad04
Eduardo Fernando Dias <dudu.dias74@gmail.com>
2019-09-07 20:02:03 -0300

    aula 4

--- aluraframe/client/js/app/models/ListaNegociacoes.js
+++ aluraframe/client/js/app/models/ListaNegociacoes.js
@@ -1,5 +1,13 @@
 class ListaNegociacoes{
+    
     constructor(){
         this._negociacoes=[]
+    }
+    adiciona(negociacao){
+        this._negociacoes.push(negociacao);
+    }
+
+    get negociacao(){
+        return [].concat(this._negociacoes);
     }
 }
