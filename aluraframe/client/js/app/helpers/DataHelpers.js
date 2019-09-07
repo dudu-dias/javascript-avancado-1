877064b6f7313fa946425fc7ac7854b328ef0be0
Eduardo Fernando Dias <dudu.dias74@gmail.com>
2019-09-07 20:16:44 -0300

    aula 4

--- aluraframe/client/js/app/helpers/DataHelpers.js
+++ aluraframe/client/js/app/helpers/DataHelpers.js
@@ -5,7 +5,7 @@ class DataHelper{
     }
 
     static textoParaData(texto){
-       if(!/\d{4}-\d{2}-\d{2}/.test(texto)){
+       if(!/^\d{4}-\d{2}-\d{2}$/.test(texto)){
             throw new Error('A data deve estar no formato AAAA-MM-DD');
        }
         return new Date(...texto.split('-').map((item, indice) => item - indice % 2));
